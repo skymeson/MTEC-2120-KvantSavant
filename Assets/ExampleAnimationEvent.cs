4bfbbacb90e7a77947f43c487f8b549ad2e1cb42
Sky Rolnick <skymeson@gmail.com>
2022-10-28 15:56:06 -0400

    Updated Animations.  Added Animation Event.

---
+++ Assets/ExampleAnimationEvent.cs
@@ -0,0 +1,11 @@
+// This C# function can be called by an Animation Event
+using UnityEngine;
+using System.Collections;
+
+public class ExampleAnimationEvent : MonoBehaviour
+{
+    public void PrintEvent(string s)
+    {
+        Debug.Log("PrintEvent: " + s + " called at: " + Time.time);
+    }
+}
